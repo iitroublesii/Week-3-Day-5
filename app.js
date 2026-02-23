b3dac3fa16acf12c3ebe2596e6fc342095fe689d
Jo Greenlee <sweettroubles29@gmail.com>
2026-02-23 17:13:25 -0600

    Added tags.forEach

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-"use strict"
+"use strict";
 
-const tags = ["js", "week 3"]
-const scores = [10, 25, 18, 30]
+const tags = ["js", "week 3"];
+const scores = [10, 25, 18, 30];
 
-let totalScore = 0
+let totalScore = 0;
 
 for (let score of scores) {
-    totalScore = totalScore + score
+  totalScore = totalScore + score;
 }
-console.log(totalScore)
+console.log(totalScore);
+
+tags.forEach(function (tag) {
+  console.log(tag);
+});
